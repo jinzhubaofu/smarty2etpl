e0db1fd9efe73d08f95d9ef3ab1bc440649193a2
leon <leonlu@outlook.com>
2014-01-12 23:19:53 +0800

    add grunt task: 1: jison parser

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function (grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-saucelabs');
+  grunt.loadNpmTasks('es6-module-packager');
+
+  grunt.task.loadTasks('tasks');
+  
+};
---
+++ src/parser-suffix.js
@@ -0,0 +1,2 @@
+export default parser;
+/* jshint ignore:end */
